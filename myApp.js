c482efa4e37d68f0027011d28f73ab05e37d663f
ryanzheng0507 <ryanzheng0507@gmail.com>
2024-11-21 23:02:45 -0800

    feat: Create a Model

--- myApp.js
+++ myApp.js
@@ -1,9 +1,34 @@
 require('dotenv').config();
+// mongoose@^5.11.15 has been added to your project’s package.json file. First, require mongoose as mongoose in myApp.js. Next, create a .env file and add a MONGO_URI variable to it. Its value should be your MongoDB Atlas database URI. Be sure to surround the URI with single or double quotes, and remember that you can't use spaces around the = in environment variables. For example, MONGO_URI='VALUE'.
+// When you are done, connect to the database by calling the connect method within your myApp.js file by using the following syntax:
+// mongoose.connect(<Your URI>, { useNewUrlParser: true, useUnifiedTopology: true });
 let mongoose = require('mongoose')
 mongoose.connect(process.env.MONGO_URI)
 
+// First of all, we need a Schema. Each schema maps to a MongoDB collection. It defines the shape of the documents within that collection. Schemas are building blocks for Models. They can be nested to create complex models, but in this case, we'll keep things simple. A model allows you to create instances of your objects, called documents.
+// Gitpod is a real server, and in real servers, the interactions with the database happen in handler functions. These functions are executed when some event happens (e.g. someone hits an endpoint on your API). We’ll follow the same approach in these exercises. The done() function is a callback that tells us that we can proceed after completing an asynchronous operation such as inserting, searching, updating, or deleting. It's following the Node convention, and should be called as done(null, data) on success, or done(err) on error.
+// Warning - When interacting with remote services, errors may occur!
+// /* Example */
+// const someFunc = function(done) {
+//   //... do something (risky) ...
+//   if (error) return done(error);
+//   done(null, result);
+// };
+// Create a person schema called personSchema with the following shape:
+// A required name field of type String
+// An age field of type Number
+// A favoriteFoods field of type [String]
+// Use the Mongoose basic schema types. If you want you can also add more fields, use simple validators like required or unique, and set default values. See our Mongoose article.
+// Now, create a model from the personSchema and assign it to the existing variable Person.
 
-let Person;
+const Schema = mongoose.Schema;
+const personSchema = new Schema({
+  name: {type: String, required: true},
+  age: Number,
+  favoriteFoods: [String]
+});
+
+let Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
